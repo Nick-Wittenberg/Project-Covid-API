5fbe29b90d2f5545149c995325334853be37e202
Nick Wittenberg <nickwittenberg@developersinstitute.co.nz>
2021-08-28 14:21:45 +1200

    Complete recordModel and pass tests

--- src/models/recordModel.js
+++ src/models/recordModel.js
@@ -4,6 +4,17 @@ const { Schema } = mongoose;
 const model = new Schema({
   _id: { type: String, required: true }, // should be `geoId-yyyymmdd` format
   /** TODO */
+  dateRep: String,
+  day: Number,
+  month: Number,
+  year: Number,
+  cases: Number,
+  deaths: Number,
+  countriesAndTerritories: String,
+  geoId: { type: String, required: true },
+  countryterritoryCode: String,
+  popData2019: Number,
+  continentExp: String,
 });
 
 export default mongoose.model("Record", model, "records");
