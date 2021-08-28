0ee1e905bee5f72059476af9d6c42fe7f3e0fc1f
Nick Wittenberg <nickwittenberg@developersinstitute.co.nz>
2021-08-28 14:30:28 +1200

    Complete Part B

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,6 +3,10 @@ const { Schema } = mongoose;
 
 const model = new Schema({
   /** TODO */
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  emailOptIn: { type: Boolean, default: false },
 });
 
 export default mongoose.model("User", model, "users");
